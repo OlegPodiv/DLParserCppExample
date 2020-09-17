5ff47bb6f1f8fb0d0456f9f19b2fd781d347431b
OlegPodiv <atman2002@list.ru>
2020-09-17 15:08:57 +0500

    новый файл:    libs/linux/include/DLParser.h 	новый файл:    libs/linux/include/Errors.h 	новый файл:    libs/windows/include/DLParser.h 	новый файл:    libs/windows/include/Errors.h 	удалено:       libs/include/DLParser.h 	удалено:       libs/include/Errors.h 	удалено:       libs/windows/win32_x86/include/DLParser.h 	удалено:       libs/windows/win32_x86/include/Errors.h 	удалено:       libs/windows/win32_x86_64/include/DLParser.h 	удалено:       libs/windows/win32_x86_64/include/Errors.h 	изменено:      main.cpp

---
+++ libs/windows/include/Errors.h
@@ -0,0 +1,48 @@
+/*
+ *  Copyright (c) 2018 IDscan.net
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  Support: support@idscan.net
+ */
+
+#ifndef IDSCAN_ERRORS_H_
+#define IDSCAN_ERRORS_H_
+
+/** Error codes */
+#define NO_ERROR 0
+#define ERR_INVALID_DATA_LEN -1
+#define ERR_INVALID_AAMVA_VERSION -7
+#define ERR_INVALID_NUMBER_OF_ENTRIES -8
+#define ERR_INVALID_SUBFILE_OFFSET -9
+#define ERR_INVALID_SUBFILE_LEN -10
+#define ERR_INVALID_INPUT_STRING -11
+#define ERR_INVALID_FILE_TYPE -14
+#define ERR_BAR_CODES_NOT_FOUND -15
+#define ERR_FIELD_NOT_FOUND -16
+#define ERR_INVALID_INPUT_STRING_LEN -17
+#define ERR_INVALID_OUTPUT_STRING_LEN -18
+#define ERR_INVALID_TIME -19
+#define ERR_INVALID_INPUT_STRING_FORMAT -20
+#define ERR_INVALID_PARAMETERS -21
+#define ERR_INVALID_PARSER_TYPE -22
+#define ERR_SUBFILE_HEADER_NOT_FOUND -23
+#define ERR_PARSE_RESULT_NOT_INIT -24
+#define ERR_STRING_NOT_PARSED -25
+#define ERR_NO_VALUE -26
+#define ERR_JNI_EXCEPTION -27
+#define ERR_INVALID_LICENSE_KEY -28
+#define ERR_INVALID_LICENSE_KEY_PACKAGE_NAME -29
+#define ERR_INVALID_LICENSE_KEY_EXPIRED -30
+
+#endif /* IDSCAN_ERRORS_H_ */
